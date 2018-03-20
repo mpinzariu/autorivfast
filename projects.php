fe6b8bf0726308a52dc1a715bb77362aa185912c
Florin Marian <mpinzariu11@gmail.com>
2018-03-20 10:06:29 +0200

    Changing html into php.

---
+++ projects.php
@@ -0,0 +1,24 @@
+<?php
+
+echo
+'
+<section id="projects" class="home-section text-center bg-gray">
+  <div class="heading-contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="wow bounceInDown" data-wow-delay="0.4s">
+            <div class="section-heading">
+              <i class="fa fa-2x fa-angle-down"></i>
+
+            </div>
+            <h2>Proiecte</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+';
+
+ ?>
